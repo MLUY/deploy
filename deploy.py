a87bae1a92a1c9420ecdc60722de86103599382e
BuildTools <unconfigured@null.spigotmc.org>
2021-06-14 14:04:49 +0100

    first commit

---
+++ deploy.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+header=st.beta_container()
+dataset=st.beta_container()
+features=st.beta_container()
+ModelTraining=st.beta_container()
+
+with header:
+    st.title('Welcome to my Boskalis DES project')
+    st.video(video_file)
+
